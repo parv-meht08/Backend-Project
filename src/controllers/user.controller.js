f6785d417c52ef180155678e229bfc64d47cc560
Parv Mehta <mehtaparv21@gmail.com>
2024-09-17 14:17:22 +0530

    Bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -168,7 +168,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized Request");
   }
 
